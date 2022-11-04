8420625d4cc178e81d1f0d4bbf82a6e58871a65f
Koll <kollpotato@gmail.com>
2022-11-04 17:20:30 +0200

    recover button component

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,35 @@
+<button
+    class="button {$$restProps.class}"
+    on:click
+    on:mouseover
+    on:focus
+    on:mouseenter
+    on:mouseleave
+    {...$$restProps}
+>
+    <slot />
+</button>
+
+<style lang="scss">
+    $blue: #4a67ff;
+
+    .button {
+        padding: 16px 14px;
+        border-radius: 6px;
+        border: none;
+        outline: none;
+        font-weight: 400;
+        font-size: 16px;
+        cursor: pointer;
+    }
+    :global(.button.blue) {
+        color: white;
+        background-color: $blue;
+    }
+    :global(.button.blue-invert) {
+        outline: solid $blue 3px;
+        outline-offset: -3px;
+        background-color: transparent;
+        color: white;
+    }
+</style>
